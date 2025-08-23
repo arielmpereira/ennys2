3fd811d3fee3a21a5dcf8e472dae5505ea468921
Nadia Santillán <nadiasantillan@gmail.com>
2025-08-23 17:58:18 -0300

    Edad en años y en meses

--- herramientas/fechas.py
+++ herramientas/fechas.py
@@ -5,3 +5,9 @@ def edad(fecha_nacimiento, fecha):
     elif fecha.month == fecha_nacimiento.month and fecha.day < fecha_nacimiento.day:
         res -= 1
     return res
+
+def edad_meses(fecha_nacimiento, fecha):
+    res = edad(fecha_nacimiento, fecha)*12 + (fecha.month - fecha_nacimiento.month)%12
+    if fecha.day < fecha_nacimiento.day:
+        res -= 1
+    return res
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from herramientas.lector import Lector
-from herramientas.fechas import edad
+from herramientas.fechas import edad, edad_meses
 
 def personas():
     l = Lector("./data/ENNyS2_encuesta.csv")
@@ -8,8 +8,10 @@ def personas():
         ("MHDR_KEY", "E_CUEST", "region", "fecha_entr", "antropo_fnac", "antropo_sex", "PESO", "TALLA", "IMC", "ZPE", "ZTE", "PTZ", "Cobertura_salud"), 
         variables_fecha=("fecha_entr", "antropo_fnac"))
     df["años_cumplidos"] = df.apply(lambda fila: edad(fila["antropo_fnac"], fila["fecha_entr"]), axis=1)
+    df["meses_cumplidos"] = df.apply(lambda fila: edad_meses(fila["antropo_fnac"], fila["fecha_entr"]), axis=1)
     print("Personas:", len(df))
-    print(df.head())
+    df_niños_pequeños = df[(df["meses_cumplidos"]>=6)&(df["meses_cumplidos"]<72)]
+    print(df_niños_pequeños.head())
 
 def alimentos():
     df = pd.read_csv("./data/Base_Alimentos_Bebidas_Suplementos.csv", low_memory=False)
